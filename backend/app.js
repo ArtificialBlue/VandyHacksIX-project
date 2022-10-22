1cd37a1e760c4c6bb143ccd8e22cc8c157e39bda
Xihan Fu <xihanfu@Xihans-MacBook-Pro.local>
2022-10-22 08:41:00 -0500

    add connect func in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,16 @@ const express = require('express');
 const app = express();
 
 // database
-const connectDB = require('./db/connect');
+const connectDB = require('./database/connect');
 
 
 const router = require('./routes/mapRoute')
 app.use('/',router)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB('mongodb+srv://vandyproj:VandyHacks9@vandyhacks9.lgq8a.mongodb.net/?retryWrites=true&w=majority');
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
