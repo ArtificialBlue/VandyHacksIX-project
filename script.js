19f42967c60c42fe296465b256296463c9d4eeef
Alexander Lopez <alexanderlopez2@Alexanders-MacBook-Air.local>
2022-10-21 22:47:15 -0500

    [Add] script.js,style.css, Basic Maps

---
+++ script.js
@@ -0,0 +1,8 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.7749, lng: -122.4194},
+        zoom: 11,
+        mapId: '92cb37b5095a990e'
+    });
+    
+}
