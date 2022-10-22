6cc4257e3e28af33c82c1b7f88d49f16f546abef
Alexander Lopez <alexanderlopez2@Alexanders-MacBook-Air.local>
2022-10-22 03:44:14 -0500

    [Add] Trimmed Data

--- Data Stuff/data_trim.py
+++ Data Stuff/data_trim.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('AIS_2022_01_29.csv')
 #70-79
-cargoVesselType = df(df['VesselType'] >= 70 and df['VesselType'] <= 79).any(axis=1)
-print(cargoVesselType.head())
+cargoVesselType = df[(df['VesselType'].between(70, 79, inclusive=True))].drop(columns=['MMSI',"SOG","COG","IMO","Status","Draft","Cargo","TransceiverClass"])
+print(cargoVesselType)
+
+cargoVesselType.to_csv('cargoVesselType.csv', index=False)
