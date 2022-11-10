7bba4ed8dee4a5b91728a4435bdde692a62c0224
Alex L <44451861+ArtificialBlue@users.noreply.github.com>
2022-11-09 22:53:42 -0800

    Placeholder Text
    Update for those wishing to use this for themselves; Google Cloud Platforms Maps ID needed.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 37.7749, lng: -122.4194},
         zoom: 5,
-        mapId: '92cb37b5095a990e'
+        mapId: '[MAP ID REQUIRED]'
     });
 
     //Needs to be a list of lists of lat and long pairs.
